22f256950d3ebe5d5d7eab949a09956da784ec8c
eventore4567 <mosbahjayden@gmail.com>
2026-08-06 23:35:17 +0200

    Charger le centre Setup complet du dashboard

--- web/__init__.py
+++ web/__init__.py
@@ -5,8 +5,10 @@ from . import embed_dashboard as _embed_dashboard
 from . import channel_search_dashboard as _channel_search_dashboard
 from . import exact_channel_match_dashboard as _exact_channel_match_dashboard
 from . import config_status_dashboard as _config_status_dashboard
+from . import setup_dashboard as _setup_dashboard
 
 _embed_dashboard.install(_dashboard)
 _channel_search_dashboard.install(_dashboard)
 _exact_channel_match_dashboard.install(_dashboard)
 _config_status_dashboard.install(_dashboard)
+_setup_dashboard.install(_dashboard)
