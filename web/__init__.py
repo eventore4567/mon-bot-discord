37e7633f5f4f8e49a1e5b78ba1761162f078b0e2
eventore4567 <mosbahjayden@gmail.com>
2026-08-06 22:36:16 +0200

    Charger la priorité des correspondances exactes

--- web/__init__.py
+++ web/__init__.py
@@ -3,6 +3,8 @@
 from . import dashboard as _dashboard
 from . import embed_dashboard as _embed_dashboard
 from . import channel_search_dashboard as _channel_search_dashboard
+from . import exact_channel_match_dashboard as _exact_channel_match_dashboard
 
 _embed_dashboard.install(_dashboard)
 _channel_search_dashboard.install(_dashboard)
+_exact_channel_match_dashboard.install(_dashboard)
