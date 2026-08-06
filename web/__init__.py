b876f99d9a828ee8cfeb5ecb1c3608f745fd59ac
eventore4567 <mosbahjayden@gmail.com>
2026-08-07 00:20:06 +0200

    Activer les listes autorisées et bloquées exclusives

--- web/__init__.py
+++ web/__init__.py
@@ -8,6 +8,7 @@ from . import dashboard as _dashboard
 from . import setup_dashboard as _setup_dashboard
 from . import design_setup_dashboard as _design_setup_dashboard
 from . import setup_center as _setup_center
+from . import setup_center_exclusive as _setup_center_exclusive
 
 
 _original_handle_index = _dashboard.handle_index
@@ -23,4 +24,5 @@ async def _handle_index_without_cache(request):
 
 
 _dashboard.handle_index = _handle_index_without_cache
+_setup_center_exclusive.install(_setup_center, _setup_dashboard)
 _setup_center.install(_dashboard, _setup_dashboard, _design_setup_dashboard)
