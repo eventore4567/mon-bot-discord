3e4be0207528de51ca9e91976f53133be67d5687
eventore4567 <mosbahjayden@gmail.com>
2026-08-26 21:46:15 +0200

    fix: activate cleaner SentriX log spacing

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,7 @@
 Le renderer compact historique est chargé en premier pour conserver la compatibilité des
 anciens cogs. La couche runtime SentriX est ensuite appliquée une seule fois : elle devient
 le point central pour le rendu, les logs et les correctifs transversaux des commandes.
+Le nettoyage final retire les séparateurs répétés et espace proprement les journaux.
 """
 
 from . import wide_compact_v6 as _wide_compact_v6
@@ -12,3 +13,7 @@ _wide_compact_v6.install()
 from . import sentrix_runtime as _sentrix_runtime
 
 _sentrix_runtime.install()
+
+from . import sentrix_visual_cleanup as _sentrix_visual_cleanup
+
+_sentrix_visual_cleanup.install()
