98633ecd0a271bceec174a96cfe4dea1c4d545d0
eventore4567 <mosbahjayden@gmail.com>
2026-08-30 13:38:39 +0200

    disable legacy log send monkeypatch during V2 diagnosis

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,9 @@
 """Utilitaires partagés de SentriX.
 
-Le renderer officiel des logs reste ``utils.log_service`` -> ``utils.wide_logs``.
-Une garde de compatibilité est installée ici afin que les très anciens appelants qui
-font encore directement ``TextChannel.send(embed=...)`` dans un salon de logs ne puissent
-plus contourner la bannière SentriX.
+Les modules de logs sont chargés explicitement par leurs consommateurs.
+
+Pendant le diagnostic Components V2, aucun monkey-patch global de
+``discord.TextChannel.send`` n'est installé ici : il masquait l'origine exacte des
+échecs en pouvant retomber sur un ancien embed. Le pipeline officiel reste
+``utils.log_service`` -> ``utils.wide_logs``.
 """
-
-from utils.log_channel_guard import install as _install_log_channel_guard
-
-_install_log_channel_guard()
-
-del _install_log_channel_guard
