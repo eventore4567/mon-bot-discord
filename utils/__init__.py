3054a867c49325a0c339cc585a4713142c2b01a6
eventore4567 <mosbahjayden@gmail.com>
2026-08-30 02:55:06 +0200

    feat(logs): enable SentriX wide log guard

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,9 +7,12 @@ y compris les anciens appelants, utilisent les mêmes bannières SentriX 1024 px
 
 from .log_banners import install as _install_log_banners
 from .log_banner_assets import install as _install_log_banner_assets
+from .log_wide_guard import install as _install_log_wide_guard
 
 _install_log_banner_assets()
+_install_log_wide_guard()
 _install_log_banners()
 
 del _install_log_banner_assets
+del _install_log_wide_guard
 del _install_log_banners
