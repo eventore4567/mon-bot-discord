0748882d26514574d0ff134fef11c761cc24fbd3
eventore4567 <mosbahjayden@gmail.com>
2026-08-31 04:35:07 +0200

    fix(commands): place native command banners above content

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,10 +7,14 @@ Le transport officiel reste ``utils.log_service`` -> ``utils.wide_logs``.
 La présentation des réponses de commandes est centralisée séparément dans
 ``utils.command_visuals``. Elle ne touche jamais ``TextChannel.send`` ni
 ``Messageable.send`` et ne peut donc pas intercepter les journaux Components V2.
+Les panneaux classiques interactifs utilisent en complément ``top_command_banners``
+pour garder la bannière SentriX au-dessus du contenu pendant toutes leurs éditions.
 """
 
 from .command_visuals import install_command_visuals
+from .top_command_banners import install_top_command_banners
 
 install_command_visuals()
+install_top_command_banners()
 
-__all__ = ["install_command_visuals"]
+__all__ = ["install_command_visuals", "install_top_command_banners"]
