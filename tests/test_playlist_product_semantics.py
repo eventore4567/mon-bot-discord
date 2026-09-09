d34b76c53702f7f440d5aa73ac1c95c08c0e2efb
eventore4567 <mosbahjayden@gmail.com>
2026-09-09 23:31:16 +0100

    test(music): assert Spotify 2026 user authorization message

--- tests/test_playlist_product_semantics.py
+++ tests/test_playlist_product_semantics.py
@@ -32,7 +32,8 @@ def test_spotify_2026_playlist_restriction_is_not_called_temporary_outage():
     )
     assert title == "Playlist Spotify non accessible"
     assert "403" in description
-    assert "possédez/collaborez" in description
+    assert "autorisation utilisateur" in description
+    assert "authentification d'application" in description
 
 
 def test_unavailable_provider_is_not_reported_as_track_missing():
