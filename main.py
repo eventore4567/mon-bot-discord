f46a95c77b3f2b536a20d6cb9fffcf4014cb7776
eventore4567 <mosbahjayden@gmail.com>
2026-09-23 21:31:45 +0100

    fix(games): classify minesweeper in main permission audit mirror

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ PUBLIC_COMMANDS = frozenset({
     "rps", "guess-number", "trivia", "tictactoe", "hangman", "math-quiz",
     "blackjack", "slots",
     "coinflip", "dice", "luckyroll", "highlow", "memory", "reaction",
-    "scramble", "wordgame", "emojiquiz", "colorquiz", "fasttype", "duel",
+    "scramble", "wordgame", "emojiquiz", "colorquiz", "fasttype", "minesweeper", "duel",
     "connect4", "numberduel", "reactionduel", "quizduel", "triviastart",
     "wordrace", "reactionevent", "guessrace", "mathrace", "lastmessage",
     "emoji-race", "adventure", "dungeon", "mining", "fishing", "treasure",
